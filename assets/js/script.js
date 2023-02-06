9c02f5d83ee32bd925ef7b7c1b03b9b70410c386
JXG052 <jxg0521@gmail.com>
2023-02-06 12:34:22 +0000

    Added submit form handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,22 @@
-const apiKey = "ab9f8caa2dd9cd7134acc296912c94ae"
-let lat;
-let lon;
 
 
-let cityName = "london"
-const geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
 
-let weatherApiRootUrl = 'https://api.openweathermap.org';
-// local variable  
 
-    
-
-  
+// This runs on submit of a form?
+$("#search-form").on("submit", function (event){
+    event.preventDefault()
+    const cityName = $("#search-input").val()
+    const apiKey = "ab9f8caa2dd9cd7134acc296912c94ae"
+    const geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+//  1st call returns lat and lon values from a given city. Those values are then used to return 5 day forecast
 $.ajax({
     url: geoCodeURL,
     method: "GET"
 }).then(function(response) {
-    
-    lat = JSON.stringify(response[0].lat);
-    lon = JSON.stringify(response[0].lon);
-    let apiUrl = weatherApiRootUrl + '/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + apiKey;
+    const weatherApiRootUrl = 'https://api.openweathermap.org';
+    let lat = JSON.stringify(response[0].lat);
+    let lon = JSON.stringify(response[0].lon);
+    const apiUrl = weatherApiRootUrl + '/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + apiKey;
 
     $.ajax({
         url: apiUrl,
@@ -28,12 +25,6 @@ $.ajax({
         console.log(newResponse)
     })
 
+    })
 })
 
-
-// $.ajax({
-//     url: apiUrl,
-//     method: "GET"
-// }).then(function(response) {
-//     console.log(response)
-// })
