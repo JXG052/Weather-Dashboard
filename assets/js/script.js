f997e898da914937df644a24d2d0da0c76c9e05e
JXG052 <jxg0521@gmail.com>
2023-02-06 12:17:59 +0000

    Added working code for 5 day weather call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,39 @@
 const apiKey = "ab9f8caa2dd9cd7134acc296912c94ae"
+let lat;
+let lon;
 
 
-// var weatherApiRootUrl = 'https://api.openweathermap.org';
-// // local variable  
-//   var apiUrl =
-//     weatherApiRootUrl +
-//     '/data/2.5/forecast?lat=' +
-//     lat +
-//     '&lon=' +
-//     lon +
-//     '&units=metric&appid=' +
-//     apiKey;
-    
-    
 let cityName = "london"
-  let geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+const geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
 
-  console.log(geoCodeURL)
+let weatherApiRootUrl = 'https://api.openweathermap.org';
+// local variable  
+
+    
+
   
+$.ajax({
+    url: geoCodeURL,
+    method: "GET"
+}).then(function(response) {
+    
+    lat = JSON.stringify(response[0].lat);
+    lon = JSON.stringify(response[0].lon);
+    let apiUrl = weatherApiRootUrl + '/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + apiKey;
+
+    $.ajax({
+        url: apiUrl,
+        method: "GET"
+    }).then(function(newResponse) {
+        console.log(newResponse)
+    })
+
+})
+
+
+// $.ajax({
+//     url: apiUrl,
+//     method: "GET"
+// }).then(function(response) {
+//     console.log(response)
+// })
