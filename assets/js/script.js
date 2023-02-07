ce5b93a8442342caff7c6bbd7f232fd16a782025
JXG052 <jxg0521@gmail.com>
2023-02-07 10:39:17 +0000

    added current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,9 @@
 let myMoment = moment()
 let today = myMoment.format("YYYY-MM-DD")
 // This runs on submit of a form?
-// $("#search-form").on("submit", function (event){
-//     event.preventDefault()
-//     const cityName = $("#search-input").val()
-//     const apiKey = "ab9f8caa2dd9cd7134acc296912c94ae"
-//     const geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
-//  1st call returns lat and lon values from a given city. Those values are then used to return 5 day forecast
-// $.ajax({
-//     url: geoCodeURL,
-//     method: "GET"
-//     }).then(function(response) {
-//         const weatherApiRootUrl = 'https://api.openweathermap.org';
-//         let lat = JSON.stringify(response[0].lat);
-//         let lon = JSON.stringify(response[0].lon);
-//         const apiUrl = weatherApiRootUrl + '/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + apiKey;
-
-//         $.ajax({
-//             url: apiUrl,
-//             method: "GET"
-//         }).then(function(finalResponse) {
-//             let icon = finalResponse.list[0].weather[0].icon
-//             let iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
-//             let weather = finalResponse.list[40]
-//             console.log(weather)
-//         })
-
-//     })
-// })
-
-
-    // const cityName = $("#search-input").val()
-    const cityName = "London" // for testing
+$("#search-form").on("submit", function (event){
+    event.preventDefault()
+    const cityName = $("#search-input").val()
     const apiKey = "ab9f8caa2dd9cd7134acc296912c94ae"
     const geoCodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
 
@@ -57,30 +29,58 @@ let today = myMoment.format("YYYY-MM-DD")
 
     })
 
-    function getWeather (response) {
-        
-        let forecast = response.list
-        console.log(forecast);
-        // let middayArray = forecast.filter(function (el){
-        //     if(el.dt_txt)
-        // })
-        let arrayOfDT = forecast.map(function (el){
-            return el.dt_txt.slice(11)
-        })
-       
-        console.log(arrayOfDT);
-        let indexofMidday = arrayOfDT.indexOf(`12:00:00`)
-        console.log(indexofMidday)
-        // let indexofMidday = 
-        // Next 5 days
-        for (let i = indexofMidday; i < 40; i+=8){
-            displayWeather(forecast[i])                      
-            }
-    }
+})
 
-//  takes an object i.e forcast[i]
-// and returns on HTML
-function displayWeather(el){
+
+
+
+// formats the response from Ajax call and calls the display function for both 5 day and today
+function getWeather (response) {
+    
+    let forecast = response.list
+    console.log(forecast);
+    // let middayArray = forecast.filter(function (el){
+    //     if(el.dt_txt)
+    // })
+    let arrayOfDT = forecast.map(function (el){
+        return el.dt_txt.slice(11)
+    })
+    
+   
+    let indexofMidday = arrayOfDT.indexOf(`12:00:00`)
+    
+    // let indexofMidday = 
+    // Next 5 days
+    for (let i = indexofMidday; i < 40; i+=8){
+        display5DayWeather(forecast[i])                      
+        }
+    displayCurrentWeather(forecast[0])
+}
+
+function displayCurrentWeather(el){
+        // Assign variables
+        let icon = (el.weather[0].icon)
+        let iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+        let temp = JSON.stringify(el.main.temp)
+        let wind = JSON.stringify(el.wind.speed)
+        let humidity = JSON.stringify(el.main.humidity)
+        let description = JSON.stringify(el.weather[0].description)
+        let date = JSON.stringify(el.dt_txt)
+        const todayCard = $("<div class='card col-2 todayCard'>Current Weather</div>")
+            // Render to HTML
+        const iconDisplay = $(`<img src="${iconURL}" alt="todaysWeather"> `)
+        const dateDisplay = $(`<p class="dateDisplay">Date and Time: ${date}</p> `)
+        const tempDisplay = $(`<p class="tempDisplay">Temperature is: ${temp}</p>`)
+        const windDisplay = $(`<p class="windDisplay">Wind is: ${wind}</p>`)
+        const humidityDisplay = $(`<p class="humidityDisplay">Humidity is: ${humidity}</p>`)
+        const descriptionDisplay = $(`<p class="description">${description}</p>`)
+
+    $("#today").append(todayCard)
+    todayCard.append([dateDisplay, iconDisplay, tempDisplay, windDisplay, humidityDisplay, descriptionDisplay])
+}
+
+// Renders weather to HTML for the 5 day forecast
+function display5DayWeather(el){
     console.log(el)
     // Assign variables
     let icon = (el.weather[0].icon)
@@ -89,7 +89,7 @@ function displayWeather(el){
     let wind = JSON.stringify(el.wind.speed)
     let humidity = JSON.stringify(el.main.humidity)
     let description = JSON.stringify(el.weather[0].description)
-    let date = JSON.stringify(el.dt_txt).split(" ")[0]
+    let date = JSON.stringify(el.dt_txt)
     const forecastCard = $("<div class='card col-2 forecastCard'></div>")
     
     // Render to HTML
@@ -107,6 +107,8 @@ function displayWeather(el){
 }
 
 
-console.log(today);
+
+
+
 
     
