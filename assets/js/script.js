65364ec1979df712427d8fb14640b9e996901abe
JXG052 <jxg0521@gmail.com>
2023-02-07 13:56:51 +0000

    Updated Buttons for historical searches and added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,36 @@
 let myMoment = moment()
 let today = myMoment.format("YYYY-MM-DD")
+let history = []
+let storedSearches = JSON.parse(localStorage.getItem("weatherHistory"))
+console.log(storedSearches);
+if (storedSearches != null){
+    history = storedSearches.map(function(search){
+        return search
+    })
+    history.forEach(element => {
+        addButton(element.cityName)
+        
+    });
+    $("#history").on("click", ".historyBtns", function(event){
+        clear()
+        let city = event.target.innerHTML.trim()
+        console.log(city)
+        history.forEach(function (element){
+            if (city === element.cityName){
+                getWeather(JSON.parse(element.data))
+            }
+        })
+        
+
+    })
+    console.log("history");
+    console.log(history);
+}
+
+
 // This runs on submit of a form?
 $("#search-form").on("submit", function (event){
+    
     event.preventDefault()
     clear()
     const cityName = $("#search-input").val().trim()
@@ -21,14 +50,20 @@ $("#search-form").on("submit", function (event){
             url: apiUrl,
             method: "GET"
         }).then(function(finalResponse) {
+            let data = JSON.stringify(finalResponse)
+            let toStore = {cityName, data}
+            history.push(toStore)
+            console.log(history);
             getWeather(finalResponse)
- 
+            localStorage.setItem("weatherHistory", JSON.stringify(history))
+            addButton(cityName)
 
                 
                // .dt_txt: "2023-02-11 12:00:00")
         })
 
     })
+    
 
 })
 
@@ -41,9 +76,10 @@ function clear() {
 
 // formats the response from Ajax call and calls the display function for both 5 day and today
 function getWeather (response) {
+    console.log(response);
     let cityName = response.city.name
     let forecast = response.list
-    console.log(forecast);
+    
     // let middayArray = forecast.filter(function (el){
     //     if(el.dt_txt)
     // })
@@ -63,14 +99,14 @@ function getWeather (response) {
 }
 
 function displayCurrentWeather(el, cityName){
-        console.log(el)
+        
         // Assign variables
         let icon = (el.weather[0].icon)
         let iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
         let temp = JSON.stringify(el.main.temp)
         let wind = JSON.stringify(el.wind.speed)
         let humidity = JSON.stringify(el.main.humidity)
-        let description = JSON.stringify(el.weather[0].description)
+        // let description = JSON.stringify(el.weather[0].description)
         let date = myMoment.format('DD/MM/YYYY')
         const todayCard = $("<div class='todayCard'></div>")
         // Render to HTML
@@ -87,7 +123,7 @@ function displayCurrentWeather(el, cityName){
 
 // Renders weather to HTML for the 5 day forecast
 function display5DayWeather(el){
-    console.log(el)
+    
     // Assign variables
     let icon = (el.weather[0].icon)
     let iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
@@ -95,18 +131,28 @@ function display5DayWeather(el){
     let wind = JSON.stringify(el.wind.speed)
     let humidity = JSON.stringify(el.main.humidity)
     let description = JSON.stringify(el.weather[0].description)
-    let date = el.dt_txt.slice(0,10)
+    let date = el.dt_txt.slice(0, 10).split("-")
+    let englishDate = []
+    date.map(function (el){
+        return englishDate.unshift(el)
+    })
+    
     const forecastCard = $("<div class='card col-2 forecastCard'></div>")
     
     // Render to HTML
     const iconDisplay = $(`<img src="${iconURL}" alt="picOfWeather"> `)
-    const dateDisplay = $(`<h5 class="dateDisplay">${date}</h5> `)
-    const tempDisplay = $(`<p class="tempDisplay">Temperature is: ${temp}</p>`)
-    const windDisplay = $(`<p class="windDisplay">Wind is: ${wind}</p>`)
-    const humidityDisplay = $(`<p class="humidityDisplay">Humidity is: ${humidity}</p>`)
-    const descriptionDisplay = $(`<p class="description">${description}</p>`)
+    const dateDisplay = $(`<h5 class="dateDisplay">${englishDate[0]}/${englishDate[1]}/${englishDate[2]}</h5> `)
+    const tempDisplay = $(`<p class="tempDisplay">Temp: ${temp} °C</p>`)
+    const windDisplay = $(`<p class="windDisplay">Wind: ${wind} KPH</p>`)
+    const humidityDisplay = $(`<p class="humidityDisplay">Humidity: ${humidity} %</p>`)
+    // const descriptionDisplay = $(`<p class="description">${description}</p>`)
     $("#forecast").append(forecastCard)
-    forecastCard.append([dateDisplay, iconDisplay, tempDisplay, windDisplay, humidityDisplay, descriptionDisplay])
+    forecastCard.append([dateDisplay, iconDisplay, tempDisplay, windDisplay, humidityDisplay])
+}
+
+function addButton(cityName) {
+    const newBtn = $(`<button data-name="${cityName}" class="historyBtns">${cityName}</button>`)
+    $("#history").append(newBtn)
 }
 
 
